84588a0e68fc26974d2e7a68227b3d3e2525f1a7
Loren Kuich <loren.jk3@gmail.com>
2023-08-30 13:06:56 -0700

    Remove client modal

--- client/pages/Clients/index.page.tsx
+++ client/pages/Clients/index.page.tsx
@@ -1,13 +1,10 @@
 import { Card } from 'primereact/card';
-import { Button } from 'primereact/button';
 
 import { Role } from 'types/roles';
 
 import { Row, Column } from 'components/Group';
-import { Modal } from 'components/Modal';
 
 import ClientList from './components/ClientList';
-import ClientForm from './components/ClientForm';
 
 function Clients() {
   return (
@@ -16,12 +13,6 @@ function Clients() {
         header={
           <Row justify="between" align="center" mx={4} mt={4}>
             <h2 className="my-0">Clients</h2>
-            <Modal
-              header="Add Client"
-              trigger={<Button label="Add Client" type="button" icon="pi pi-plus" />}
-            >
-              <ClientForm />
-            </Modal>
           </Row>
         }
       >
