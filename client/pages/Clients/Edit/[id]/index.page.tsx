1fa7e8fe069468551d6bf0bece5b6ddb8bbf6e68
dsaglam94 <d.saglam@outlook.fr>
2023-08-31 21:35:57 +0300

    implementing dynamic fetch for single client

---
+++ client/pages/Clients/Edit/[id]/index.page.tsx
@@ -0,0 +1,26 @@
+import { useQuery } from '@apollo/client';
+import { useRouter } from 'next/router';
+
+import ClientForm from '../../components/ClientForm';
+
+import clientQuery from '../queries/client.gql';
+
+export default function EditClient() {
+  const router = useRouter();
+  const { id: clientId } = router.query;
+
+  const { data: client } = useQuery(clientQuery, {
+    variables: {
+      id: clientId
+    }
+  });
+
+  console.log('single client:: ', client);
+
+  return (
+    <div>
+      <h1>Edit Client</h1>
+      <ClientForm />
+    </div>
+  );
+}
