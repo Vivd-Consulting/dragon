37a9798b2d9a28b348055e7abb79a12aa08b0337
dsaglam94 <d.saglam@outlook.fr>
2023-09-01 19:52:01 +0300

    adding add client button for navigation

--- client/pages/Clients/index.page.tsx
+++ client/pages/Clients/index.page.tsx
@@ -1,18 +1,29 @@
-import { Card } from 'primereact/card';
+import { useRouter } from 'next/router';
 
-import { Role } from 'types/roles';
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
 
 import { Row, Column } from 'components/Group';
+
+import { Role } from 'types/roles';
 
 import ClientList from './components/ClientList';
 
 function Clients() {
+  const router = useRouter();
+
   return (
     <Column gap="4" fullWidth>
       <Card
         header={
           <Row justify="between" align="center" mx={4} mt={4}>
             <h2 className="my-0">Clients</h2>
+            <Button
+              onClick={() => router.push('/clients/create')}
+              label="Add Client"
+              type="button"
+              icon="pi pi-plus"
+            />
           </Row>
         }
       >
