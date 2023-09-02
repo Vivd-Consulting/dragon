1e126eff888bd848be9410566c9b2146a5bdeeef
Loren Kuich <loren.jk3@gmail.com>
2023-09-02 00:30:24 -0700

    Fixed logout issue

--- client/pages/api/auth/logout.page.js
+++ client/pages/api/auth/logout.page.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
-  const returnTo = encodeURI([process.env.NEXTAUTH_URL]);
+  const uri = process.env.NEXTAUTH_URL || '';
 
   res.redirect(
-    `${process.env.AUTH0_URI}/v2/logout?federated&returnTo=${encodeURIComponent(
-      returnTo
-    )}&client_id=${process.env.AUTH0_CLIENT}`
+    `${process.env.AUTH0_URI}/v2/logout?federated&returnTo=${encodeURIComponent(uri)}&client_id=${
+      process.env.AUTH0_CLIENT
+    }`
   );
 }
