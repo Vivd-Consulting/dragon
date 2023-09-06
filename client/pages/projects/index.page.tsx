0276a8b08670d8f68fe943e4ac6b4c8ecae4fe4b
dsaglam94 <d.saglam@outlook.fr>
2023-09-06 15:10:49 +0300

    showing project list on page

---
+++ client/pages/projects/index.page.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from 'next/router';
+
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+
+import { Role } from 'types/roles';
+
+import { Row, Column } from 'components/Group';
+
+import ProjectList from './components/ProjectsList';
+
+export default function Projects() {
+  const router = useRouter();
+
+  return (
+    <Column gap="4" fullWidth>
+      <Card
+        header={
+          <Column>
+            <Row justify="between" align="center" mx={4} mt={4}>
+              <h2 className="my-0">Projects</h2>
+              <Button
+                onClick={() => router.push('/projects/create')}
+                label="Add Project"
+                type="button"
+                icon="pi pi-plus"
+                raised
+              />
+            </Row>
+            {/* TODO: Add Filter Inputs */}
+            <Row justify="between" align="center" mx={4} mt={4}>
+              hello
+            </Row>
+          </Column>
+        }
+      >
+        <ProjectList />
+      </Card>
+    </Column>
+  );
+}
+
+Projects.roles = [Role.Admin];
