be3bb64af141c02bc55ce7e66b32a07ef1436490
Loren Kuich <loren.jk3@gmail.com>
2023-09-08 11:24:36 -0700

    Restored clients

---
+++ client/pages/clients/index.page.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from 'next/router';
+
+import { Card } from 'primereact/card';
+import { Button } from 'primereact/button';
+
+import { Role } from 'types/roles';
+
+import { Row, Column } from 'components/Group';
+
+import ClientList from './components/ClientList';
+
+export default function Clients() {
+  const router = useRouter();
+
+  return (
+    <Column gap="4" fullWidth>
+      <Card
+        header={
+          <Row justify="between" align="center" mx={4} mt={4}>
+            <h2 className="my-0">Clients</h2>
+            <Button
+              onClick={() => router.push('/clients/create')}
+              label="Add Client"
+              type="button"
+              icon="pi pi-plus"
+              raised
+            />
+          </Row>
+        }
+      >
+        <ClientList />
+      </Card>
+    </Column>
+  );
+}
+
+Clients.roles = [Role.Admin];
