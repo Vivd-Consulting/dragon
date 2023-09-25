92b8ef920401e66b91f3c81440fdd2c92f8ac025
dsaglam94 <d.saglam@outlook.fr>
2023-09-25 21:58:11 +0300

    moving invoice & invoice_item tables

--- hasura/migrations/default/1690919783812_resources/up.sql
+++ hasura/migrations/default/1690919783812_resources/up.sql
@@ -52,36 +52,6 @@ CREATE TABLE contractor (
   updated_at timestamp NOT NULL DEFAULT now()
 );
 
-CREATE TABLE invoice (
-  id SERIAL PRIMARY KEY,
-
-  client_id integer REFERENCES client(id),
-  contractor_id integer REFERENCES contractor(id),
-
-  submitted_at timestamp,
-  due_date timestamp,
-  paid_at timestamp,
-
-  archived_at timestamp,
-
-  created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now()
-);
-
-CREATE TABLE invoice_item (
-  id SERIAL PRIMARY KEY,
-  description text,
-  currency text NOT NULL DEFAULT 'CAD',
-  price float NOT NULL,
-  tax float NOT NULL,
-
-  invoice_id integer REFERENCES invoice(id),
-
-  deleted_at timestamp,
-  created_at timestamp NOT NULL DEFAULT now(),
-  updated_at timestamp NOT NULL DEFAULT now()
-);
-
 CREATE TABLE dragon_user (
   id text NOT NULL PRIMARY KEY,
   name text NOT NULL,
