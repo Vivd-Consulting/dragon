8097b48f408eeb3cf286d4fca8142211d4dc4c3c
dsaglam94 <d.saglam@outlook.fr>
2023-09-28 00:48:22 +0300

    no more rounding for hours

--- client/utils/calculateTotalTimeAndCost.ts
+++ client/utils/calculateTotalTimeAndCost.ts
@@ -1,47 +1,26 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
 
-function roundUpToHalfHour(minutes) {
-  const halfHour = 30;
+dayjs.extend(duration);
 
-  return Math.ceil(minutes / halfHour) * halfHour;
-}
+export default function calculateTotalTimeAndCost(timeArray, rate = 0) {
+  let totalTime = dayjs.duration(0);
+  let totalCost = 0;
 
-function formatAsHoursAndMinutes(minutes) {
-  if (Number.isNaN(minutes)) {
-    return '--:--:--';
-  }
+  timeArray.forEach(entry => {
+    const startTime = dayjs(entry.start_time);
+    const endTime = dayjs(entry.end_time);
 
-  const hours = Math.floor(minutes / 60);
-  const remainingMinutes = minutes % 60;
+    const entryDuration = endTime.diff(startTime);
 
-  return `${hours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}`;
-}
+    totalTime = totalTime.add(entryDuration);
 
-// Helper function to calculate time spent for a single pair
-function calculateTimeSpent(start, end) {
-  const startTime = dayjs(start);
-  const endTime = dayjs(end);
-  const durationMinutes = endTime.diff(startTime, 'minutes');
+    const entryCost = (entryDuration / (60 * 60 * 1000)) * rate;
 
-  return roundUpToHalfHour(durationMinutes);
-}
+    totalCost += entryCost;
+  });
 
-// Main function to calculate total time spent
-export default function calculateTotalTimeAndCost(timePairs, rate = 0) {
-  let totalMinutesSpent = 0;
+  const formattedTotalTime = `${totalTime.hours()}:${totalTime.minutes()}:${totalTime.seconds()}`;
 
-  for (const pair of timePairs) {
-    const { start_time, end_time } = pair;
-
-    totalMinutesSpent += calculateTimeSpent(start_time, end_time);
-  }
-
-  const totalTime = formatAsHoursAndMinutes(totalMinutesSpent);
-  const totalHours = totalMinutesSpent / 60;
-  const totalCost = rate * totalHours;
-
-  return {
-    totalTime,
-    totalCost
-  };
+  return { totalTime: formattedTotalTime, totalCost };
 }
