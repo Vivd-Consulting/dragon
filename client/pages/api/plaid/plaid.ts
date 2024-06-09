07e652e3c9fda831f687ef740b8eb9127a60a083
Loren Kuich <loren.jk3@gmail.com>
2024-06-08 20:26:39 -0700

    Simplify sync

--- client/pages/api/plaid/plaid.ts
+++ client/pages/api/plaid/plaid.ts
@@ -29,141 +29,146 @@ export async function backfillTransactions() {
   console.log('Accounts:', accounts.length);
 
   for (const account of accounts) {
-    const knexTransaction = await knex.transaction();
     const { token, cursor } = account;
 
-    try {
-      // Get all transactions for this account
-      const transaction = await fetchTransactions({ token, cursor });
-      const { added, removed, modified, cursor: lastCursor } = transaction;
+    let hasMore = true;
 
-      if (added.length > 0) {
-        // Insert the transactions into the database
-        const insertData = await Promise.all(
-          added.map(async (transaction: any) => {
-            const credit = transaction.amount < 0 ? transaction.amount : null;
-            const debit = transaction.amount > 0 ? transaction.amount : null;
+    while (hasMore) {
+      const knexTransaction = await knex.transaction();
 
-            const isDebit = debit && debit > 0;
-            const type = isDebit ? 'DEBIT' : 'CREDIT';
+      try {
+        // Get all transactions for this account
+        const transaction = await fetchTransactions({
+          token,
+          cursor
+        });
+        const { added, removed, modified, cursor: lastCursor, hasMore: _hasMore } = transaction;
 
-            const rule = await getRule({
-              name: transaction.name,
-              account_id: transaction.account_id,
-              type
-            });
+        hasMore = _hasMore;
 
-            return {
-              account_id: transaction.account_id,
-              account_owner: transaction.account_owner,
-              credit,
-              debit,
-              date: transaction.datetime || transaction.date,
-              currency: transaction.iso_currency_code,
-              location: transaction.location,
-              logo_url: transaction.logo_url,
-              merchant_name: transaction.merchant_name,
-              name: transaction.name,
-              payment_channel: transaction.payment_channel,
-              payment_meta: transaction.payment_meta,
-              personal_finance_category: transaction.detailed,
-              personal_finance_category_confidence: transaction.confidence_level,
-              personal_finance_category_icon_url: transaction.personal_finance_category_icon_url,
-              id: transaction.transaction_id,
-              transaction_type: transaction.transaction_type,
-              website: transaction.website,
-              counterparties: transaction.counterparties,
-              category_id: transaction.category_id,
-              applied_rule: rule?.id,
-              category: transaction.category,
-              gic_category_id: rule?.gic_id,
-              tax_id: rule?.tax_id
-            };
-          })
-        );
+        if (added.length > 0) {
+          // Insert the transactions into the database
+          const insertData = await Promise.all(
+            added.map(async (transaction: any) => {
+              const credit = transaction.amount < 0 ? transaction.amount : null;
+              const debit = transaction.amount > 0 ? transaction.amount : null;
 
-        if (insertData.length > 0) {
-          await knex('accounting.transactions')
-            .insert(insertData)
-            .onConflict(['id'])
-            .merge()
-            .transacting(knexTransaction);
-        } else {
-          console.warn('No data to insert for account:', account.id);
+              const isDebit = debit && debit > 0;
+              const type = isDebit ? 'DEBIT' : 'CREDIT';
+
+              const rule = await getRule({
+                name: transaction.name,
+                account_id: transaction.account_id,
+                type
+              });
+
+              return {
+                account_id: transaction.account_id,
+                account_owner: transaction.account_owner,
+                credit,
+                debit,
+                date: transaction.datetime || transaction.date,
+                currency: transaction.iso_currency_code,
+                location: transaction.location,
+                logo_url: transaction.logo_url,
+                merchant_name: transaction.merchant_name,
+                name: transaction.name,
+                payment_channel: transaction.payment_channel,
+                payment_meta: transaction.payment_meta,
+                personal_finance_category: transaction.detailed,
+                personal_finance_category_confidence: transaction.confidence_level,
+                personal_finance_category_icon_url: transaction.personal_finance_category_icon_url,
+                id: transaction.transaction_id,
+                transaction_type: transaction.transaction_type,
+                website: transaction.website,
+                counterparties: transaction.counterparties,
+                category_id: transaction.category_id,
+                applied_rule: rule?.id,
+                category: transaction.category,
+                gic_category_id: rule?.gic_id,
+                tax_id: rule?.tax_id
+              };
+            })
+          );
+
+          if (insertData.length > 0) {
+            await knex('accounting.transactions')
+              .insert(insertData)
+              .onConflict(['id'])
+              .merge()
+              .transacting(knexTransaction);
+          } else {
+            console.warn('No data to insert for account:', account.id);
+          }
         }
-      }
 
-      if (modified.length > 0) {
-        const updateData = modified.map((transaction: any) => ({
-          account_id: transaction.account_id,
-          account_owner: transaction.account_owner,
-          credit: transaction.amount < 0 ? transaction.amount : null,
-          debit: transaction.amount > 0 ? transaction.amount : null,
-          date: transaction.datetime || transaction.date,
-          currency: transaction.iso_currency_code,
-          location: transaction.location,
-          logo_url: transaction.logo_url,
-          merchant_name: transaction.merchant_name,
-          name: transaction.name,
-          payment_channel: transaction.payment_channel,
-          payment_meta: transaction.payment_meta,
-          personal_finance_category: transaction.detailed,
-          personal_finance_category_confidence: transaction.confidence_level,
-          personal_finance_category_icon_url: transaction.personal_finance_category_icon_url,
-          id: transaction.transaction_id,
-          transaction_type: transaction.transaction_type,
-          website: transaction.website,
-          counterparties: transaction.counterparties,
-          category_id: transaction.category_id,
-          category: transaction.category,
-          updated_at: new Date()
-        }));
+        if (modified.length > 0) {
+          const updateData = modified.map((transaction: any) => ({
+            account_id: transaction.account_id,
+            account_owner: transaction.account_owner,
+            credit: transaction.amount < 0 ? transaction.amount : null,
+            debit: transaction.amount > 0 ? transaction.amount : null,
+            date: transaction.datetime || transaction.date,
+            currency: transaction.iso_currency_code,
+            location: transaction.location,
+            logo_url: transaction.logo_url,
+            merchant_name: transaction.merchant_name,
+            name: transaction.name,
+            payment_channel: transaction.payment_channel,
+            payment_meta: transaction.payment_meta,
+            personal_finance_category: transaction.detailed,
+            personal_finance_category_confidence: transaction.confidence_level,
+            personal_finance_category_icon_url: transaction.personal_finance_category_icon_url,
+            id: transaction.transaction_id,
+            transaction_type: transaction.transaction_type,
+            website: transaction.website,
+            counterparties: transaction.counterparties,
+            category_id: transaction.category_id,
+            category: transaction.category,
+            updated_at: new Date()
+          }));
 
-        if (updateData.length > 0) {
-          await knex('accounting.transactions')
-            .update(updateData)
-            .whereIn(
-              'id',
-              modified.map((transaction: any) => transaction.transaction_id)
-            )
-            .returning('*')
-            .transacting(knexTransaction);
-        } else {
-          console.warn('No data to update for account:', account.id);
+          if (updateData.length > 0) {
+            await knex('accounting.transactions')
+              .update(updateData)
+              .whereIn(
+                'id',
+                modified.map((transaction: any) => transaction.transaction_id)
+              )
+              .returning('*')
+              .transacting(knexTransaction);
+          } else {
+            console.warn('No data to update for account:', account.id);
+          }
         }
-      }
 
-      if (removed.length > 0) {
-        const removeIds = removed.map((transaction: any) => transaction.transaction_id);
+        if (removed.length > 0) {
+          const removeIds = removed.map((transaction: any) => transaction.transaction_id);
 
-        if (removeIds.length > 0) {
-          await knex('accounting.transactions')
-            .delete()
-            .whereIn('id', removeIds)
-            .transacting(knexTransaction);
-        } else {
-          console.warn('No data to remove for account:', account.id);
+          if (removeIds.length > 0) {
+            await knex('accounting.transactions')
+              .delete()
+              .whereIn('id', removeIds)
+              .transacting(knexTransaction);
+          } else {
+            console.warn('No data to remove for account:', account.id);
+          }
         }
+
+        // Update the cursor
+        await knex('accounting.bank')
+          .update({ cursor: lastCursor, updated_at: new Date() })
+          .where({ token })
+          .transacting(knexTransaction);
+
+        await knexTransaction.commit();
+      } catch (error: any) {
+        await knexTransaction.rollback();
+        await knex('accounting.bank').update({ error: error.message }).where({ token });
+        console.error('Error processing account:', account.id, error);
+
+        break;
       }
-
-      // Update the cursor
-      await knex('accounting.bank')
-        .update({ cursor: lastCursor, updated_at: new Date() })
-        .where({ token })
-        .transacting(knexTransaction);
-
-      console.log({
-        account: account.id,
-        added: added.length,
-        modified: modified.length,
-        removed: removed.length
-      });
-
-      await knexTransaction.commit();
-    } catch (error: any) {
-      await knexTransaction.rollback();
-      await knex('accounting.bank').update({ error: error.message }).where({ token });
-      console.error('Error processing account:', account.id, error);
     }
   }
 }
@@ -187,52 +192,37 @@ export async function getCategories() {
 }
 
 async function fetchTransactions({ token, cursor }) {
+  // Iterate through each page of new transaction updates for item
+  const request = {
+    access_token: token,
+    cursor: cursor
+  };
+
+  const response = await client.transactionsSync(request);
+
+  if (response.status !== 200) {
+    console.error(response);
+    throw new Error(response.statusText);
+  }
+
+  const data = response.data;
+
   // New transaction updates since "cursor"
-  let added: any[] = [];
-  let modified: any[] = [];
+  const added: any[] = data.added;
+  const modified: any[] = data.modified;
 
   // Removed transaction ids
-  let removed: any[] = [];
-  let hasMore = true;
+  const removed: any[] = data.removed;
+  const hasMore = data.has_more;
 
-  // Iterate through each page of new transaction updates for item
-  while (hasMore) {
-    const request = {
-      access_token: token,
-      cursor: cursor
-    };
-
-    const response = await client.transactionsSync(request);
-
-    if (response.status !== 200) {
-      console.error(response);
-      throw new Error(response.statusText);
-    }
-
-    const data = response.data;
-
-    // Add this page of results
-    added = added.concat(data.added);
-    modified = modified.concat(data.modified);
-    removed = removed.concat(data.removed);
-    hasMore = data.has_more;
-    // Update cursor to the next cursor
-    cursor = data.next_cursor;
-
-    // await knex('accounting.bank')
-    //   .update({
-    //     cursor,
-    //     updated_at: new Date()
-    //   })
-    //   .where({ token })
-    //   .transacting(knexTransaction);
-  }
+  cursor = data.next_cursor;
 
   return {
     added,
     removed,
     modified,
-    cursor
+    cursor,
+    hasMore
   };
 }
 
